20811495c768f9fa1c3456c784fe1480826592ac
Adailton Gama Santana <76784673+Adailton-Gama@users.noreply.github.com>
2022-08-17 22:14:05 -0300

    modified:   FLUTTER/todo_list/lib/pages/todo_list_page.dart

--- FLUTTER/todo_list/lib/pages/todo_list_page.dart
+++ FLUTTER/todo_list/lib/pages/todo_list_page.dart
@@ -9,35 +9,32 @@ import '../models/todo.dart';
 
 class TodoListPage extends StatefulWidget {
   TodoListPage({Key? key}) : super(key: key);
-  
+
   @override
   State<TodoListPage> createState() => _TodoListPageState();
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-
   final TextEditingController task = TextEditingController();
   final TextEditingController editText = TextEditingController();
   final TodoRepository todoRepository = TodoRepository();
-  
 
-  List<Todo> todos= [];
+  List<Todo> todos = [];
   int count = 0;
   Todo? offDelete;
   int? offDeletePos;
-  int pendente=0;
-  int completos=0;
+  int pendente = 0;
+  int completos = 0;
 
   @override
   void initState() {
     super.initState();
     todoRepository.getTodoList().then((value) {
       setState(() {
-       todos = value;
-       count = todos.length;
+        todos = value;
+        count = todos.length;
       });
     });
-
   }
 
   @override
@@ -92,16 +89,16 @@ class _TodoListPageState extends State<TodoListPage> {
                 Flexible(
                   child: ListView(
                     shrinkWrap: true,
-                      children: [
-                        for(Todo todo in todos)
+                    children: [
+                      for (Todo todo in todos)
                         ItemTodoList(
-                          todo: todo, 
+                          todo: todo,
                           onDelete: onDelete,
                           onUpdate: onUpdate,
                           onComplete: onComplete,
-                        )                                              
-                      ],
-                    ),
+                        )
+                    ],
+                  ),
                 ),
                 const SizedBox(height: 10),
                 Row(
@@ -136,10 +133,18 @@ class _TodoListPageState extends State<TodoListPage> {
       print('Nenhuma tarefa adicionada');
       Vibration.vibrate();
       ScaffoldMessenger.of(context).clearSnackBars();
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Favor preencher a tarefa',textAlign: TextAlign.center,),backgroundColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Favor preencher a tarefa',
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+        ),
+      );
     } else {
       print('Tarefa $getTask adicionada!');
-      setState(() { 
+      setState(() {
         Todo newTodo = Todo(
           title: getTask,
           dataTime: DateTime.now(),
@@ -153,17 +158,25 @@ class _TodoListPageState extends State<TodoListPage> {
       task.clear();
     }
   }
-  void addTaskSubmited(String getTask){
+
+  void addTaskSubmited(String getTask) {
     String getTask = task.text;
     if (getTask == '') {
       print('Nenhuma tarefa adicionada');
       Vibration.vibrate();
       ScaffoldMessenger.of(context).clearSnackBars();
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Favor preencher a tarefa!',textAlign: TextAlign.center,),backgroundColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          'Favor preencher a tarefa!',
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+      ));
     } else {
       print('Tarefa $getTask adicionada!');
       setState(() {
-        Todo newTodo = Todo(title: task.text, dataTime: DateTime.now(),complete: false);
+        Todo newTodo =
+            Todo(title: task.text, dataTime: DateTime.now(), complete: false);
         todos.add(newTodo);
         count = todos.length;
         print('indice: $count, da tarefa: $getTask');
@@ -172,6 +185,7 @@ class _TodoListPageState extends State<TodoListPage> {
       task.clear();
     }
   }
+
   void clearList() {
     List<Todo> backupTodo = todos;
     setState(() {
@@ -185,32 +199,31 @@ class _TodoListPageState extends State<TodoListPage> {
                     style: ElevatedButton.styleFrom(
                       primary: Colors.red[400],
                     ),
-                      onPressed: () {
-                        setState(() {
+                    onPressed: () {
+                      setState(() {
                         todos.clear();
                         count = todos.length;
                         todoRepository.saveTodoList(todos);
-                        });
-                        Navigator.pop(context);
-                      },
-                      child: Text('Sim'),
-                      ),
-
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
-                    ),
-                    onPressed: () {
+                      });
                       Navigator.pop(context);
                     },
-                    child: Text('Não')),
+                    child: Text('Sim'),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Não')),
                 ],
               ));
     });
     task.clear();
   }
-  
-  void onDelete(Todo todo){
+
+  void onDelete(Todo todo) {
     offDelete = todo;
     offDeletePos = todos.indexOf(todo);
     setState(() {
@@ -224,23 +237,23 @@ class _TodoListPageState extends State<TodoListPage> {
         backgroundColor: Colors.grey[300],
         content: Text(
           'A tarefa ${todo.title} foi removida com sucesso!',
-          style: const TextStyle(color:  Colors.black),
-          ),
+          style: const TextStyle(color: Colors.black),
+        ),
         action: SnackBarAction(
           label: 'Desfazer',
-          textColor:  Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
-          onPressed: (){
+          textColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+          onPressed: () {
             setState(() {
-            todos.insert(offDeletePos!, offDelete!);
-            todoRepository.saveTodoList(todos);
+              todos.insert(offDeletePos!, offDelete!);
+              todoRepository.saveTodoList(todos);
             });
           },
-          ),
         ),
+      ),
     );
   }
-  
-  void onUpdate(Todo todo){
+
+  void onUpdate(Todo todo) {
     setState(() {
       print(todo.title);
     });
@@ -250,48 +263,50 @@ class _TodoListPageState extends State<TodoListPage> {
           return AlertDialog(
             title: Text('Editar'),
             content: TextField(
-              controller: editText,
-              decoration: const InputDecoration(
-                hintText: 'Digite a nova tarefa',
-                border: OutlineInputBorder(),
+                controller: editText,
+                decoration: const InputDecoration(
+                  hintText: 'Digite a nova tarefa',
+                  border: OutlineInputBorder(),
                 ),
-                onSubmitted: (String text){
-                setState(() {
-                    if(editText.text == ''){
-                      Vibration.vibrate();
-                      ScaffoldMessenger.of(context).clearSnackBars();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          backgroundColor:Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
-                          content: Text('Favor preencher a tarefa que deseja alterar!'),
-                          duration: Duration(seconds: 3),
-                          ),
-                          );
-
-                    }else{
-                      todo.title = editText.text;
-                      todo.dataTime = DateTime.now();
-                      editText.clear();      
-                      todoRepository.saveTodoList(todos);
-                      Navigator.pop(context);
-                      }
-              }, 
-            );
-            }
-            ),                           
-            actions: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
-                ),
-                  onPressed: () {
-                    setState(() {
-                       if (editText.text == '') {
-                         Vibration.vibrate();
-                         ScaffoldMessenger.of(context).clearSnackBars();
+                onSubmitted: (String text) {
+                  setState(
+                    () {
+                      if (editText.text == '') {
+                        Vibration.vibrate();
+                        ScaffoldMessenger.of(context).clearSnackBars();
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
-                            backgroundColor: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+                            backgroundColor:
+                                Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+                            content: Text(
+                                'Favor preencher a tarefa que deseja alterar!'),
+                            duration: Duration(seconds: 3),
+                          ),
+                        );
+                      } else {
+                        todo.title = editText.text;
+                        todo.dataTime = DateTime.now();
+                        editText.clear();
+                        todoRepository.saveTodoList(todos);
+                        Navigator.pop(context);
+                      }
+                    },
+                  );
+                }),
+            actions: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (editText.text == '') {
+                        Vibration.vibrate();
+                        ScaffoldMessenger.of(context).clearSnackBars();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            backgroundColor:
+                                Color.fromRGBO(0x12, 0xCB, 0xE0, 1),
                             content: Text(
                                 'Favor preencher a tarefa que deseja alterar!'),
                             duration: Duration(seconds: 3),
@@ -306,34 +321,34 @@ class _TodoListPageState extends State<TodoListPage> {
                       }
                     });
                   },
-                  child: Text('Editar')
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: Colors.red[400]),
-                    onPressed: (){
-                      editText.clear();
+                  child: Text('Editar')),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.red[400]),
+                  onPressed: () {
+                    editText.clear();
                     Navigator.pop(context);
-                    }, child: Text('Cancelar'))
+                  },
+                  child: Text('Cancelar'))
             ],
-            
           );
         });
   }
 
-  void onComplete(Todo todo){    
-   bool oncomplete = false;   
-   setState(() {
-     if(todo.complete==true){
-     todo.complete = false; 
-      oncomplete = false;
-     }else{
-       todo.complete =true;
-      oncomplete = false;
-     };
-     todoRepository.saveTodoList(todos);
-   });
-  for(Todo todo in todos){
-    print(todo.complete);
-  }
+  void onComplete(Todo todo) {
+    bool oncomplete = false;
+    setState(() {
+      if (todo.complete == true) {
+        todo.complete = false;
+        oncomplete = false;
+      } else {
+        todo.complete = true;
+        oncomplete = false;
+      }
+      ;
+      todoRepository.saveTodoList(todos);
+    });
+    for (Todo todo in todos) {
+      print(todo.complete);
+    }
   }
 }
